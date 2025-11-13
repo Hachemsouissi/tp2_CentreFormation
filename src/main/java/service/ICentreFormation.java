5c3e524cbc5a9ec370b379409d2076f08e1728c7
Hachem <hachemsouissi1@gmail.com>
2025-11-13 22:34:52 +0100

    3

--- src/main/java/service/ICentreFormation.java
+++ src/main/java/service/ICentreFormation.java
@@ -6,7 +6,11 @@ import jakarta.jws.WebService;
 @WebService
 public interface ICentreFormation {
     @WebMethod
-    String inscrireStagiaire(@WebParam(name = "nom") String nom, @WebParam(name = "prenom") String prenom, @WebParam(name = "formation") String formation);
+    String inscrireStagiaire(@WebParam(name = "nom") String nom,
+                             @WebParam(name = "prenom") String prenom,
+                             @WebParam(name = "formation") String formation);
     @WebMethod
-    double calculerMoyenne(@WebParam(name = "note1") double note1, @WebParam(name = "note2") double note2, @WebParam(name = "note3") double note3);
+    double calculerMoyenne(@WebParam(name = "note1") double note1,
+                           @WebParam(name = "note2") double note2,
+                           @WebParam(name = "note3") double note3);
 }
